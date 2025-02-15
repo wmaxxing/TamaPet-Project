412b4c7ff7f5fe947da3eaef9ad66c63be9906e8
mwalkley <mwalkley@student.cs.ubc.ca>
2025-02-15 14:48:26 -0800

    Added Specifications for the FoodMenu class

--- src/main/model/FoodMenu.java
+++ src/main/model/FoodMenu.java
@@ -1,8 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+
 // The FoodMenu class will represent a list of TamaFood that will allow for easy
 // viewing of what foods are available to the TamaPet
+// The FoodMenu will be statically typed as adding food to it is not a user feature
 
 public class FoodMenu {
 
+    private ArrayList<TamaFood> foodList;
+
+    //Effects: Creates a FoodMenu object which has a statically typed list of TamaFood objects
+    public FoodMenu() {
+        
+    }
+
+    //Requires that foodList.size() > index >= 0
+    //Effects: Returns the food object in FoodMenu which is at the provided index used for testing
+    public TamaFood getTamaFood(int index) {
+
+    }
+
+    //Requires: findfood is not null
+    //Effects: takes a string of the name of a TamaFood and returns the corresponding TamaFood object
+    // from the FoodMenu list
+    public TamaFood findFood(String findfood) {
+        
+    }
 }
