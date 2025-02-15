8f68ee2b71124457bed3681b9e0fe001c66d4a97
mwalkley <mwalkley@student.cs.ubc.ca>
2025-02-14 23:26:30 -0800

    Added Specifictions for the TamaFood Class

--- src/main/model/TamaFood.java
+++ src/main/model/TamaFood.java
@@ -5,4 +5,28 @@ package model;
 
 public class TamaFood {
 
+    private String name;
+    private String happienessValue;
+    private int nutritionalValue;
+
+    //Requires: That name is not null
+    //Effects: Creates an instance of the TamaFood object
+    public TamaFood(String name, int happienessValue, int nutritionalValue) {
+
+    }
+
+    //Effects: returns the name of the TamaFood object
+    public String getName() {
+
+    }
+
+    //Effects: returns the happieness value of the TamaFood object
+    public int getHappieness() {
+
+    }
+
+    //Effects: returns the nutritional value of the TamaFood object
+    public int getNutrition() {
+
+    }
 }
