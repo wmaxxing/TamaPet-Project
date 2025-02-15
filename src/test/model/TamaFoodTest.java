2315a484ee68fbdc6f4f79cb704c17e32d152059
mwalkley <mwalkley@student.cs.ubc.ca>
2025-02-14 23:33:23 -0800

    Added test for the TamaFood class

---
+++ src/test/model/TamaFoodTest.java
@@ -0,0 +1,40 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TamaFoodTest {
+    
+    TamaFood testfood;
+
+    @BeforeEach
+    void runBefore() {
+        testfood = new TamaFood("Kimchi", 10, 10); 
+    }
+
+    @Test
+    public void constructortest() {
+        TamaFood constest = new TamaFood("Kimchi", 10, 10);
+        assertEquals(constest, testfood);
+    }
+
+    @Test 
+    public void getnametest() {
+        assertEquals(testfood.getName(), "Kimchi");
+    }
+
+    @Test 
+    public void gethappienesstest() {
+        assertEquals(testfood.getHappieness(), 10);
+    }
+    
+    @Test 
+    public void getnutritiontest() {
+        assertEquals(testfood.getNutrition(), 10);
+    }
+}
