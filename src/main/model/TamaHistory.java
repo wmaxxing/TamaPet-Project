b1195f5bfabbd9d52baa3a7e64bce0e64d884dfb
mwalkley <mwalkley@student.cs.ubc.ca>
2025-02-15 13:28:14 -0800

    Added specifications for the TamaHistory Class

--- src/main/model/TamaHistory.java
+++ src/main/model/TamaHistory.java
@@ -1,9 +1,34 @@
 package model;
 
+import java.time.*;
+
 // The TamaHistory class will represent a single interaction event with the TamaPet
-// A TamaHistory will have a number to represent its place in the HistoryLog,
-// a date, a time, and a string memo detailing what happened during the event
+// A TamaHistory will have a name, a date, a time, and a string to say what happened 
+// during the interaction with the tama pet
 
 public class TamaHistory {
 
+    private String name; 
+    private String eventDescription;
+    private LocalDateTime dateAndTime;
+
+    //Effects: Creats a TamaHistory object
+    public TamaHistory(String name, String eventDescription) {
+
+    }
+
+    //Effects: Returns the name of the TamaHistory Object
+    public String getName() {
+
+    }
+
+    //Effects: Returns the event description of the TamaHistory Object
+    public String getName() {
+
+    }
+
+    //Effects: Returns the date and time of the TamaHistory Object
+    public String getName() {
+
+    }
 }
