92b665ad832276e74844dfc90a6281875fdd1855
mwalkley <mwalkley@student.cs.ubc.ca>
2025-02-15 16:38:57 -0800

    Added specifications for HistoryLog

--- src/main/model/HistoryLog.java
+++ src/main/model/HistoryLog.java
@@ -1,8 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+
 // The HistoryLog class respersents a list of TamaHistory events that will show a log of
 // all interactions with the TamaPet
 
 public class HistoryLog {
 
+    private ArrayList<TamaHistory> historyList;
+
+    //Effects: Creates a History Log object that stores all histories for the TamaPet
+    public HistoryLog() {
+        historyList = new ArrayList<TamaHistory>();
+    }
+
+    //Effects: returns the TamaHistory element at index of the list
+    public TamaHistory getTamaHistory(int index) {
+
+    }
+
+    //Effects: Adds an element to the HistoryLog
+    public void addTamaHistory(TamaHistory add) {
+        
+    }
+    
 }
